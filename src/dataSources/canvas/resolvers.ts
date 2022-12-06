5161b27edca657de4af98bab4a4209f304a5ee85
Thijmen Brand <thijmen@ik.nu>
2022-12-06 14:56:13 +0100

    Pipeline (#11)
    * Initial setup for apollo server
    
    * Implement canvas api with authorization
    
    * Add logging and express middleware
    
    * Fix express middelware
    
    * Add datasources dynamically
    
    * Remove .idea folder
    
    * Very small cleanup
    
    * Add watch mockdata
    
    * Add watch mockdata
    
    * Update README.md
    
    * Update README.md
    
    * typescript omzetten
    
    * omzetten van js naar ts
    
    * changed everything to ts
    
    * Volledig omgezet naar ts
    
    * typeDef.ts restructure
    
    * Add Assignments & Submissions typeDefs
    
    * Add support for assignmentdata and submission data
    
    * Add support for assignmentdata and submission data
    
    * Fix tsconfig
    
    * Configure Pipeline
    
    * Update jenkins.yml
    
    * Rename jenkins.yml to Jenkinsfile
    
    * Fix kubernetes deployment
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update README.md
    
    * Update pipeline
    
    * Update README.md
    
    Co-authored-by: Walter <waltersajtos@outlook.com>
    Co-authored-by: Koen Janssen <6256259+koen253janssen@users.noreply.github.com>
    Co-authored-by: Stijn Verhagen <stijn@moonlysoftware.com>
    Co-authored-by: Marijn van Dijk <marijn.vandijk@student.fontys.nl>
    Co-authored-by: Marijn van Dijk <81915434+MarijnvDijk@users.noreply.github.com>

--- src/dataSources/canvas/resolvers.ts
+++ src/dataSources/canvas/resolvers.ts
@@ -1,28 +1,28 @@
 const resolvers = {
-  Query: {
-      CanvasCourses: (parent: any, args: any, { dataSources }: any) => {
-          return dataSources.canvasData.getAllCourses()
-      },
-      CanvasCourse: (parent: any, {courseId}: any, { dataSources }: any) => {
-          return dataSources.canvasData.getSingleCourse(courseId)
-      }
-  },
-  CanvasCourse: {
-      assignments: (parent: any, args: any, { dataSources }: any) => {
-          return dataSources.canvasData.getAssignmentData(parent.id);
-      },
-      enrollments: (parent: any, args: any, { dataSources }: any) => {
-          return dataSources.canvasData.getEnrollmentData(parent.id, parent.userId);
-      }
-  },
-  Assignment: {
-      submissions: (parent: any, args: any, { dataSources }: any) => {
-          return dataSources.canvasData.getSubmissionData(parent.course_id, parent.id);
-      },
-      outcomes: (parent: any, args: any, { dataSources }: any) => {
-          return dataSources.canvasData.getOutcomesData(parent.course_id, parent.id);
-      }
-  }
+    Query: {
+        CanvasCourses: (parent: any, args: any, { dataSources }: any) => {
+            return dataSources.canvasData.getAllCourses()
+        },
+        CanvasCourse: (parent: any, {courseId}: any, { dataSources }: any) => {
+            return dataSources.canvasData.getSingleCourse(courseId)
+        }
+    },
+    CanvasCourse: {
+        assignments: (parent: any, args: any, { dataSources }: any) => {
+            return dataSources.canvasData.getAssignmentData(parent.id);
+        },
+        enrollments: (parent: any, args: any, { dataSources }: any) => {
+            return dataSources.canvasData.getEnrollmentData(parent.id, parent.userId);
+        }
+    },
+    Assignment: {
+        submissions: (parent: any, args: any, { dataSources }: any) => {
+            return dataSources.canvasData.getSubmissionData(parent.course_id, parent.id);
+        },
+        outcomes: (parent: any, args: any, { dataSources }: any) => {
+            return dataSources.canvasData.getOutcomesData(parent.course_id, parent.id);
+        }
+    }
 };
 
 export default resolvers;
