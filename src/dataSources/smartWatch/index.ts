2d6d860dfc2c93d9839e4a7ce6040d8841e763f2
Marijn van Dijk <81915434+MarijnvDijk@users.noreply.github.com>
2022-12-06 14:34:18 +0100

    Delete Unchecked Code (#8)
    Co-authored-by: Marijn van Dijk <marijn.vandijk@student.fontys.nl>

--- src/dataSources/smartWatch/index.ts
+++
@@ -1,6 +0,0 @@
-import DataSource from "../datatypes/datasource";
-import typeDef from "./typeDef";
-import resolvers from "./resolvers";
-import WatchDataApi from "./watch-api";
-
-export default new DataSource("watchData", WatchDataApi, resolvers, typeDef);
